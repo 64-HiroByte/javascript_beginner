10b6920cad51d15b882300e352b1fc1479dcde16
64-HiroByte <higher.ground.rc@gmail.com>
2025-07-30 18:57:04 +0900

    JavaScript入門・完全版」クラスとインスタンスについて学習した。

---
+++ youtube_class/accordion.js
@@ -0,0 +1,49 @@
+(() => {
+  class Accordion {
+    // 初期化
+    constructor(obj) {
+
+
+      const $elm = document.querySelector(obj.hookName);
+      const $trigger = $elm.getElementsByTagName(obj.tagName);
+
+      const triggerLen =$trigger.length;
+      let index = 0;
+      while (index < triggerLen) {
+        $trigger[index].addEventListener('click', (e) => this.clickHandler(e));
+        index++;
+      };
+    };
+
+    // クリックしたら実行される処理
+    clickHandler = (e) => {  // クラス内に関数を書く場合は const 不要
+      e.preventDefault();
+  
+      const $target = e.currentTarget;
+      const $content = $target.nextElementSibling;
+  
+      if ($content.style.display === 'block') {
+        $content.style.display = 'none';
+      } else {
+        $content.style.display = 'block';
+      };
+    };
+  };
+
+  // インスタンスの生成
+  const fuckingAccordion = new Accordion({
+    hookName: '#js-faq',
+    tagName: 'p'
+  });
+
+  const hiraganaAccordion = new Accordion({
+    hookName: '#js-accordion',
+    tagName: 'a'
+  });
+  
+  const miniAccordion = new Accordion({
+    hookName: '#js-accordion-mini',
+    tagName: 'a'
+  });
+
+})();
